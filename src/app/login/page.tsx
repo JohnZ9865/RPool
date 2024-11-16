e5ad4838aa904359d35d12a0b41dc3dd629fa30c
John Zhou <zhou9865@gmail.com>
2024-11-16 09:12:15 -0800

    login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import { api } from "@/utils/api";
+import toast from "react-hot-toast";
+
+const Home = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+        Our Login screen
+    </div>
+  );
+};
+
+export default Home;
